8780357061661146eafac650afcea94b503b9a37
Caio Eduardo Ragazzi Gemignani <ca.ragazzi@gmail.com>
2020-09-23 08:00:21 -0300

    properties descriptions

--- src/company/company.entity.ts
+++ src/company/company.entity.ts
@@ -1,12 +1,15 @@
 import { Entity, Column, PrimaryGeneratedColumn, ManyToOne } from 'typeorm';
-import { IsInt, IsNotEmpty, isNumber, IsString } from 'class-validator';
+import { IsInt, IsNotEmpty, IsString } from 'class-validator';
 import { User } from 'src/user/user.entity';
+import { ApiProperty } from '@nestjs/swagger';
 
 @Entity()
 export class Company {
+    @ApiProperty()
     @PrimaryGeneratedColumn()
     id: number;
 
+    @ApiProperty()
     @IsNotEmpty()
     @IsString()
     @Column({ length: 500 })
--- src/user/user.entity.ts
+++ src/user/user.entity.ts
@@ -1,28 +1,34 @@
 import { Entity, Column, PrimaryGeneratedColumn, Index, OneToMany } from 'typeorm';
 import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
 import { Company } from 'src/company/company.entity';
+import { ApiProperty } from '@nestjs/swagger';
 
 @Entity()
 export class User {
+  @ApiProperty()
   @PrimaryGeneratedColumn()
   id: number;
 
+  @ApiProperty()
   @IsNotEmpty()
   @IsString()
   @Column({ length: 500 })
   name: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   @MinLength(6)
   @Column('varchar')
   password: string;
 
+  @ApiProperty()
   @IsEmail()
   @IsNotEmpty()
   @Column('varchar')
   @Index({ unique: true })
   email: string;
 
+  @ApiProperty()
   @OneToMany(type => Company, company => company.user)
   company: Company[]
 }
